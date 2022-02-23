e91be3393f3ab13250a4403d45f9895a3d94be44
zDR9YHt5ooXU <git1@kaiguo.ca>
2022-02-23 15:19:13 -0800

    remove xaxis

--- index.js
+++ index.js
@@ -41,56 +41,6 @@ const data = Array.from(
     .values()
 ).sort((a, b) => d3.ascending(a.date, b.date));
 
-const xAxis = (g) =>
-  g
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', 10)
-    .call((g) =>
-      g
-        .selectAll('g')
-        .data(x.ticks())
-        .join('g')
-        .each((d, i) => (d.id = uid('month')))
-        .call((g) =>
-          g
-            .append('path')
-            .attr('stroke', '#000')
-            .attr('stroke-opacity', 0.2)
-            .attr(
-              'd',
-              (d) => `
-              M${d3.pointRadial(x(d), innerRadius)}
-              L${d3.pointRadial(x(d), outerRadius)}
-            `
-            )
-        )
-        .call((g) =>
-          g
-            .append('path')
-            .attr('id', (d) => d.id.id)
-            .datum((d) => [d, d3.utcMonth.offset(d, 1)])
-            .attr('fill', 'none')
-            .attr(
-              'd',
-              ([a, b]) => `
-              M${d3.pointRadial(x(a), innerRadius)}
-              A${innerRadius},${innerRadius} 0,0,1 ${d3.pointRadial(
-                x(b),
-                innerRadius
-              )}
-            `
-            )
-        )
-        .call((g) =>
-          g
-            .append('text')
-            .append('textPath')
-            .attr('startOffset', 6)
-            .attr('xlink:href', (d) => d.id.href)
-            .text(d3.utcFormat('%B'))
-        )
-    );
-
 const yAxis = (g) =>
   g
     .attr('text-anchor', 'middle')
@@ -127,10 +77,6 @@ const yAxis = (g) =>
             .attr('stroke', 'none')
         )
     );
-const x = d3
-  .scaleUtc()
-  .domain([Date.UTC(2000, 0, 1), Date.UTC(2001, 0, 1) - 1])
-  .range([0, 2 * Math.PI]);
 const y = d3
   .scaleLinear()
   .domain([d3.min(data, (d) => d.minmin), d3.max(data, (d) => d.maxmax)])
@@ -168,8 +114,6 @@ function radialAreaChart() {
     .attr('stroke', 'steelblue')
     .attr('stroke-width', 1.5);
   // .attr('d', line.radius((d) => y(d.avg))(data));
-
-  svg.append('g').call(xAxis);
 
   svg.append('g').call(yAxis);
 
